675fecbbc84eb0040c9314af79d2cc65049d48f0
Liliyalexx <liliya.fedyurina@gmail.com>
2024-05-07 22:10:44 -0700

    pages added

--- blog_client/src/App.tsx
+++ blog_client/src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" element={<Home/>} />
+      <Route path = "/about" element={<About/>} />
+      <Route path = "/dashboard" element={<Dashboard/>} />
+      <Route path = "/signIn" element={<SignIn/>} />
+      <Route path = "/signUp" element={<SignUp/>} />
+      <Route path = "/projects" element={<Projects/>} />
+ 
+     
+
+
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
