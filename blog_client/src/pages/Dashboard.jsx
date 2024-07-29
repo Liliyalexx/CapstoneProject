03d6d707adc36ab38a9cef9ae0303ca261a72c79
Liliyalexx <liliya.fedyurina@gmail.com>
2024-07-29 14:15:22 -0700

    Dashpoard-Post started

--- blog_client/src/pages/Dashboard.jsx
+++ blog_client/src/pages/Dashboard.jsx
@@ -16,13 +16,15 @@ export default function Dashboard() {
   }
   }, [location.search])
    return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className='md:w-56'>
-        {/* Sidebar */}
-        <DashSideBar />
-      </div>
-      {/* profile... */}
-      {tab === 'profile' && <DashProfile />}
-    </div>
+     <div className='min-h-screen flex flex-col md:flex-row'>
+       <div className='md:w-56'>
+         {/* Sidebar */}
+         <DashSideBar />
+       </div>
+       {/* profile... */}
+       {tab === 'profile' && <DashProfile />}
+       {/* posts... */}
+       {tab === 'posts' && <DashPosts />}
+     </div>
    );
 }
